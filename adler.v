cf896b8a6248e040c74a29df6f08928ddc0e8409
Zachary Murray <zwmurray@grendel29.ece.ncsu.edu>
2021-10-02 22:19:47 -0400

    Update project scope for clarity

--- adler.v
+++ adler.v
@@ -1,6 +1,18 @@
 // ECE310 Project 2 
 // Zachary Murray
-// 04/27/2021
+//
+// Adler-32 Checksum Generator -- A size input is given constantly, and is
+// meant to be captured when the size_valid signal is asserted. Once
+// data_start is asserted, the system must begin counting down from the
+// original size and adding each number to create the correct Adler-32
+// Checksum. After the data is fully received (designated by the size parameter),
+// the system must assert a "valid" signal coincident with the correct
+// checksum within 10 clock cycles. This design completes the checksum and
+// asserts "valid" within 2 clock cycles.
+//
+// Submitted on 04/27/2021.
+//
+// Main design work was completed before this date.
 
 module adler32 (
   input clk, rst_n,
